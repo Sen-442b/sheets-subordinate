6daf9541500f3144ac0b7372861b5e0d2e72f81c
Sen-442b <67971019+Sen-442b@users.noreply.github.com>
2022-11-15 21:00:01 +0530

    fix: deleted dev file import

--- client/src/utils/getClientId.js
+++ client/src/utils/getClientId.js
@@ -1,8 +1,3 @@
-import { clientId } from "../devEnv";
-
 export const getClientId = () => {
-  if (import.meta.env.MODE === "development") {
-    return clientId;
-  }
   return import.meta.env.VITE_CLIENT_ID;
 };
